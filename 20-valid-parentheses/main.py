5a53b84edaa97f0856e9802fcdf9a1eb52284a21
parsa <parsa.sedigh78@gmail.com>
2023-06-17 23:20:59 +0330

    feat: solve problem #20

---
+++ 20-valid-parentheses/main.py
@@ -0,0 +1,25 @@
+class Solution:
+	def isValid(self, s: str) -> bool:
+		stack = []
+		closeToOpen = {
+			")": "(",
+			"]": "[",
+			"}": "{"
+		}
+
+		for c in str:
+
+			# check if this character is a closing char(the keys of closeToOpen is a closing char)
+			if c in closeToOpen:
+				# The character is a closing char, so make sure the stack is not empty, because we can't have a closing
+				# char in an empty stack. Also we wanna make sure the value at the top of the stack is the matching opening
+				# char.
+				if stack and stack[-1] == closeToOpen[c]:
+					stack.pop()
+				else:
+					return False
+
+			else:
+				stack.append(c)
+
+		return True if not stack else False
