1772df9a81fc8528d7fc66c3243b3d77c0a7e0e1
parsa <parsa.sedigh78@gmail.com>
2023-07-15 21:15:29 +0330

    docs: add some comments in #19

--- 19-remove-nth-node-from-end-of-list/main.py
+++ 19-remove-nth-node-from-end-of-list/main.py
@@ -11,10 +11,12 @@ class Solution:
         left = dummy
         right = head
 
+        # first put a distance equal to n between left and right
         while n >0 and right:
             right = right.next
             n -= 1
 
+        # we need to remove nth node from the END. So move left and right until we reach the end of the linked list
         while right:
             left = left.next
             right = right.next
