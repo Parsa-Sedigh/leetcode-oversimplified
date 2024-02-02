d7e8013ce3d961f8a7c7e7b12b4c396359bd6eaa
parsa <parsa.sedigh78@gmail.com>
2024-02-02 12:18:53 +0330

    refactor: make #146 more readable

--- 146-LRU-Cache/main.py
+++ 146-LRU-Cache/main.py
@@ -25,7 +25,7 @@ class LRUCache:
 	# remove from doubly linked list
 	# Look at 146-1 img.
 	# Note: node is the middle node
-	def remove(self, node):
+	def removeNode(self, node):
 		prev, nxt = node.prev, node.next
 
 		# By doing this, the node is no longer in the middle of prev and nxt(it doesn't exist in the linked list anymore)
@@ -34,7 +34,7 @@ class LRUCache:
 	# insert node at the right of doubly linked right before the right dummy node(so that it becomes the most recently used which is pointed by
 	# right dummy node)
 	# Look at 146-2 img.
-	def insert(self, node):
+	def insertNodeAtEnd(self, node):
 		prev, nxt = self.right.prev, self.right
 
 		# insert node at the middle of prev and nxt
@@ -49,8 +49,8 @@ class LRUCache:
 			# remove the node related to this key from linked list and re-insert it at the right most position
 			# Note: The value of the key in hashmap is a pointer to the respective node and we know that the remove and insert
 			# methods want a node in their param. So pass that val which is a pointer to the node.
-			self.remove(self.cache[key])
-			self.insert(self.cache[key])
+			self.removeNode(self.cache[key])
+			self.insertNodeAtEnd(self.cache[key])
 
 			return self.cache[key].val
 
@@ -58,20 +58,23 @@ class LRUCache:
 
 	def put(self, key: int, value: int) -> None:
 		if key in self.cache:
-			self.remove(self.cache[key])
+			self.removeNode(self.cache[key])
 
 		# with this, the value of the key in hashmap is a pointer to the new node(but this node is not in the linked list yet)
 		self.cache[key] = Node(key, value)
 
 		# we also need to insert the new node in the list
-		self.insert(self.cache[key])
+		self.insertNodeAtEnd(self.cache[key])
 
 		if len(self.cache) > self.cap:
-			# remove the LRU from linked list and also remove or evict it from the hashmap(cache)
+			# remove the `lru` from linked list and also remove or evict it from the hashmap(cache)
 			lru = self.left.next
-			self.remove(lru)
+			self.removeNode(lru)
 
-			# This is why we stored not only the value but also the key, in the Node class
+			# This is why we stored not only the value but also the key, in the Node class Note: We wanna remove the
+			# first node in the linked list. It's key is stored inside itself. Do not make the mistake that the key of
+			# the first node that we wanna remove, is the `key` param of this func! The key param of this func is
+			# the key we wanna insert, but the key of the first node is stored inside itself(it's `lru.key`)
 			del self.cache[lru.key]
 
 
