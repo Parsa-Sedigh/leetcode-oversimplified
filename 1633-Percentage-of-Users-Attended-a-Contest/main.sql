2101259df466a63a00cf863605895ff6b265b7a6
parsa <parsa.sedigh78@gmail.com>
2024-04-11 18:45:19 +0330

    feat: solve #1633

---
+++ 1633-Percentage-of-Users-Attended-a-Contest/main.sql
@@ -0,0 +1,7 @@
+select contest_id,
+       round(count(distinct user_id) * 100 /
+             (select count(*)
+              from users)::decimal, 2) as percentage
+from register
+group by contest_id
+order by percentage desc, contest_id;
