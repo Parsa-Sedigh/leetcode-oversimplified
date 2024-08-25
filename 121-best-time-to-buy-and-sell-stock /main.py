6c63b16c6a161a74ea5de51dad1698a17cafdc45
Parsa Sedigh <parsa.sedigh78@gmail.com>
2024-08-25 11:55:50 +0330

    feat: solve #121 with go

--- 121-best-time-to-buy-and-sell-stock /main.py
+++ 121-best-time-to-buy-and-sell-stock /main.py
@@ -15,7 +15,7 @@ class Solution:
 				# we don't just want to shift the l pointer by 1, we want to shift it all the way to the r, because we found
 				# a really low price, we found the lowest price until now. We want our `l` to be at the minimum. So put it where `r` is at the moment.
 				# Note: When we encounter this block, it means all the values after `l` and before `r`, are less than value of `l`, because we crossed
-				# them and moved to the next price(if it wasn't the case, we already have encountered this block at that moment!). Thefore,
+				# them and moved to the next price(if it wasn't the case, we already have encountered this block at that moment!). Therefore,
 				# instead of just moving `l` by one price, put it where `r` is.
 				l = r
 
