2c865614cb281b3f39ae1c34d5fe56b2e41edb91
Parsa Sedigh <parsa.sedigh78@gmail.com>
2025-07-05 17:09:15 +0330

    feat: solve #75

--- 75-Sort-Colors/main.py
+++ 75-Sort-Colors/main.py
@@ -1,50 +1,81 @@
 from typing import List
 
+
+# 1. Counting Sort
 # Time: O(n)
 # Space: O(1)
-class Solution1:
-	def sortColors(self, nums: List[int]) -> None:
-		"""
-		Do not return anything, modify nums in-place instead.
-		"""
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        # The idx represents the actual color(val)
+        # The val represents the frequency of that color
+        # For example [2, 3, 2] means:
+        # - there are 2 instances of of color 0
+        # - 3 instances of color 1
+        # - 2 instances of color 2
+        counts = [0] * 3
 
-		l, r = 0, len(nums) - 1
-		i = 0
+        for num in nums:
+            counts[num] += 1
 
-		def swap(i, j):
-			tmp = nums[i]
-			nums[i] = nums[j]
-			nums[j] = tmp
+        # Initialize position where the next color will be written
+        write_pos = 0
 
-		while i <= r:
-			if nums[i] == 0:
-				swap(l, i)
-				l += 1
+        # since we're getting indexes, it means we're getting the color values
+        for color_val in range(len(counts)):
+            while counts[color_val]:
+                nums[write_pos] = color_val
 
-			elif nums[i] == 2:
-				swap(i, r)
-				r -= 1
-
-				# we did this because we have a i+=1 executed for all the cases, but we don't want to increment i if we encountered
-				# a 2. So we do this to compensate.
-				i -= 1
-
-			i += 1
+                counts[color_val] -= 1
+                write_pos += 1
 
 
-class Solution2:
-	def sortColors(self, nums: List[int]) -> None:
-		low = 0
-		high = len(nums) - 1
-		mid = 0
+# 2. Three Pointers - I
+# Time: O(n)
+# Space: O(1)
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
 
-		while mid <= high:
-			if nums[mid] == 0:
-				nums[low], nums[mid] = nums[mid], nums[low]
-				low += 1
-				mid += 1
-			elif nums[mid] == 1:
-				mid +=1
-			else:
-				nums[mid], nums[high] = nums[high], nums[mid]
-				high -= 1
+        # l: where next 0 goes, r: where next 2 goes
+        l, r = 0, len(nums) - 1
+
+        # i: current element being checked
+        i = 0
+
+        def swap(i: int, j: int):
+            tmp = nums[i]
+            nums[i] = nums[j]
+            nums[j] = tmp
+
+        # Continue until i PASSES r (all elements processed)
+        while i <= r:
+            if nums[i] == 0:
+                swap(l, i)
+
+                # The el at l is in it's right place, so move l to point to the place where the next 0 goes.
+                l += 1
+
+                # Move i forward since swapped element is now in correct place.
+                i += 1
+            elif nums[i] == 2:
+                # Found a 2: swap it to the right boundary (r) and move r left
+                swap(i, r)
+
+                # NOTE: We shouldn't incr `i`, because we grabbed an el from the right side of the list(pointer by r) and
+                # swapped it with an el on left side(i). We don't know where the el that was on the right should live ultimately.
+                # So we can't increment i, because we need to find where the el that we brought to i, should live. It's not decided yet.
+
+                # NOTE: This is different than when we encounter a 0. Because in that case we swap it with an el on the LEFT side of
+                # the list and we know everything on the left(up until l) is sorted. So the el at `i` is actually in it's right place.
+                r -= 1
+            else:
+                # Found a 1: it's in the right "middle" section, just move i forward
+                i += 1
+
+# 4. Three Pointers - II
+# TODO neet
+
+# 5. Three Pointers - III
+# TODO neet
